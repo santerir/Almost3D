4c7d46d2fdd6e63821048926a0592152b9437438
Santeri Räisänen <santerir@cs.helsinki.fi>
2015-03-13 20:05:05 +0200

    NB projekti luotu

---
+++ Almost3d/src/main/java/almost3d/almost3d/App.java
@@ -0,0 +1,13 @@
+package almost3d.almost3d;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
